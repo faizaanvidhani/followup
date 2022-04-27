66266e768e2be5863cffd85372fe73ffa4be5746
Faizaan Vidhani <faizaanvidhani@gmail.com>
2022-04-27 16:01:39 -0400

    feat: working firebase authentication

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { signInWithGoogle, signOut } from "./Firebase";
+import './components/login.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+            <button className="login" onClick={signInWithGoogle}>Sign In With Google</button>
+        </div>
+        <br/>
+        <div>
+            <button className="login" onClick={signOut}>Sign Out</button>
+        </div>
     </div>
   );
 }
