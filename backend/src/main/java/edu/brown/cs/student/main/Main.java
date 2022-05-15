678bce3e27730de429320dabaf2876661e6d4c30
ihuang8 <78018775+ihuang8@users.noreply.github.com>
2022-05-14 23:53:52 -0400

    refactor: fix conflict

--- backend/src/main/java/edu/brown/cs/student/main/Main.java
+++ backend/src/main/java/edu/brown/cs/student/main/Main.java
@@ -93,7 +93,7 @@ public final class Main {
     Spark.post("/provider-data", new ProviderDataHandler());
     Spark.post("/patient-data", new PatientDataHandler());
     Spark.post("/symptom-data", new SymptomDataHandler());
-    Spark.get("/table-data", new TableHandler());
+    Spark.post("/generic-table-data", new TableHandler());
     Spark.post("/insert-symptom", new SymptomInsertHandler());
     Spark.init();
   }
